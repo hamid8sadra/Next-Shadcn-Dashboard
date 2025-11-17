2f2f676e3836d9ecb9527f5b6b9d05f64baf1f91
hamid8sadra <hamid8sadra@gmal.com>
2025-11-17 18:37:26 +0330

    navbar and sidebar

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,11 @@
 import { Button } from "@/components/ui/button";
-import { CirclePlus } from "lucide-react";
+import { CalendarPlus, CirclePlus } from "lucide-react";
 import React from 'react'
 
 const Home = () => {
   return (
     <div className="h-screen flex items-center justify-center">
-      <Button variant="default" className="rounded-full" size="lg">
-        <CirclePlus/>
-        click me
-      </Button>
+
     </div>
   )
 }
--- components/AppSidebar.tsx
+++ components/AppSidebar.tsx
@@ -1,8 +1,94 @@
 import React from 'react'
+import {Home,Inbox,Calendar,Search,Settings, User, ChevronUp, User2, Github} from 'lucide-react'
+import { Sidebar, SidebarContent, SidebarFooter, SidebarGroup, SidebarGroupContent, SidebarGroupLabel, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from './ui/sidebar'
+import Link from 'next/link'
+import Image from 'next/image'
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuSubTrigger, DropdownMenuTrigger } from './ui/dropdown-menu'
+
+const items = [
+  {
+    title:"Home",
+    url:"/",
+    icon:Home,
+  },
+  {
+    title:"Inbox",
+    url:"#",
+    icon:Inbox,
+  },
+  {
+    title:"Calendar",
+    url:"#",
+    icon:Calendar,
+  },
+  {
+    title:"Search",
+    url:"#",
+    icon:Search,
+  },
+  {
+    title:"Settings",
+    url:"#",
+    icon:Settings,
+  },
+]
 
 const AppSidebar = () => {
   return (
-    <div>AppSidebar</div>
+    <Sidebar collapsible='icon'>
+      {/* Header */}
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton asChild>
+              <Link href="/" className='text-green-700'>
+                {/* <Image src="@/public/window.svg"  alt='logo' width={20} height={20}/> */}
+                <Github /> Hamid Sadra
+              </Link>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      {/* Content */}
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map(item=>
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <Link href={item.url}>
+                      <item.icon/>
+                      <span>{item.title}</span>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              )}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+      {/* Footer */}
+      <SidebarFooter>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <SidebarMenuButton>
+                  <User2/>hamid sadra<ChevronUp className='ml-auto'/>
+                </SidebarMenuButton>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align='end'>
+                <DropdownMenuItem>Account</DropdownMenuItem>
+                <DropdownMenuItem>Settings</DropdownMenuItem>
+                <DropdownMenuItem>Sign Out</DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarFooter>
+    </Sidebar>
   )
 }
 
